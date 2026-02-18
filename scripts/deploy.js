f8af34b786dac4a2baa4eebcb6afb5d7a48c5bdc
Vadym <95903575+goldgorb@users.noreply.github.com>
2026-02-18 14:58:59 +0200

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -49,3 +49,11 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+----------
+  // We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
